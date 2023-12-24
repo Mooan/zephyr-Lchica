1962388ec9d36d38644d47b2e312303cdd08b3b8
Koji YASUDA <koji.yasuda.cc@gmail.com>
2023-12-24 12:23:16 +0900

    feat: extended GPIO support
    - Extended GPIO support has been added to the DTS (Device Tree Source).
    - Assigned GPIOs to Pin 2 and Pin 5 on the Teensy board.
    - Improved the Shell interface to handle multiple GPIOs.

--- src/main.c
+++ src/main.c
@@ -8,56 +8,110 @@
 #if !DT_NODE_EXISTS(DT_NODELABEL(board_led))
 #error "led0 node not properly defined."
 #endif
-static const struct gpio_dt_spec board_led =
-	GPIO_DT_SPEC_GET_OR(DT_NODELABEL(board_led), gpios, {0});
+// 拡張したGPIOの設定を取得する
+#if !DT_NODE_EXISTS(DT_NODELABEL(board_gpio))
+#error "extended_gpio node not properly defined."
+#endif
 
-// LEDの状態を変更するコマンド
-static int cmd_led_toggle(const struct shell *shell, size_t argc, char **argv)
+// デバイスツリーのGPIO設定を取得する
+const struct gpio_dt_spec gpio_dt_specs[] = {
+    GPIO_DT_SPEC_GET_OR(DT_NODELABEL(board_led), gpios, {0}),
+    GPIO_DT_SPEC_GET_BY_IDX_OR(DT_NODELABEL(board_gpio), gpios, 0, {0}),
+    GPIO_DT_SPEC_GET_BY_IDX_OR(DT_NODELABEL(board_gpio), gpios, 1, {0})
+};
+
+// GPIOを初期化する
+static int initialize_gpio(const struct gpio_dt_spec *spec, gpio_flags_t extra_flags)
 {
-    static bool toggle = false;
-    static bool initialized = false;
     int err;
 
-    // 初回(initializedがfalse)の場合はGPIO OUTの初期化処理を行う
-    if(!initialized) { 
-        if (!gpio_is_ready_dt(&board_led)) {
-            printf("The load switch pin GPIO port is not ready.\n");
-            return 0;
-        }
-
-        printf("Initializing pin with inactive level.\n");
-
-        err = gpio_pin_configure_dt(&board_led, GPIO_OUTPUT_INACTIVE);
-        if (err != 0) {
-            printf("Configuring GPIO pin failed: %d\n", err);
-            return 0;
-        }
-
-        toggle = false;
-        err = gpio_pin_set_dt(&board_led, toggle);
-        if (err != 0) {
-            printf("Setting GPIO pin level failed: %d\n", err);
-        }
-        initialized = true;
-    } else {
-        toggle = !toggle;
+    if (!gpio_is_ready_dt(spec)) {
+        printf("The load switch pin GPIO port is not ready.\n");
+        return -1;
     }
 
-    // LEDの状態を変更する
-    printf("Toggling LED to %s\n", toggle ? "on" : "off");
-    err = gpio_pin_set_dt(&board_led, toggle);
+    printf("Initializing pin with inactive level.\n");
+
+    err = gpio_pin_configure_dt(spec, extra_flags);
     if (err != 0) {
-        printf("Setting GPIO pin level failed: %d\n", err);
+        printf("Configuring GPIO pin failed: %d\n", err);
+        return -1;
     }
 
     return 0;
 }
 
-// コマンド登録
-SHELL_CMD_REGISTER(toggle,NULL, "Toggle LED", cmd_led_toggle);
+// すべてのGPIOを初期化する
+static int initialize_all_gpios(void)
+{
+    int err;
 
+    for(int i = 0; i < ARRAY_SIZE(gpio_dt_specs); i++) {
+        err = initialize_gpio(&gpio_dt_specs[i], GPIO_OUTPUT_ACTIVE);
+        if (err != 0) {
+            printf("Initializing GPIO pin failed: %d\n", err);
+            return err;
+        }
+    }
+
+    return 0;
+}
+
+// GPIOの状態を変更するコマンド
+static int cmd_gpio_toggle(const struct shell *shell, size_t argc, char **argv)
+{
+    long idx;
+    int err;
+    const struct gpio_dt_spec *spec;
+    gpio_pin_t value;
+
+    // コマンドライン引数をチェックする
+    if (argc != 2) {
+        printf("Usage: %s <gpio_idx>\n", argv[0]);
+        return -1;
+    }
+    idx = strtol(argv[1], NULL, 10);
+    if( idx < 0 || idx >= ARRAY_SIZE(gpio_dt_specs) ) {
+        printf("Invalid GPIO index: %ld\n", idx);
+        return -1;
+    }
+
+    // コマンドラインのIDXから対応するGPIO設定を取得する
+    spec = &gpio_dt_specs[idx];
+
+    // GPIOの状態を変更する
+    value = gpio_pin_get_dt(spec);
+    if (value != 0 && value != 1) {
+        printf("Getting GPIO pin level failed: %d\n", value);
+        return -1;
+    }
+
+    value = !value;
+    
+    err = gpio_pin_set_dt(spec, value);
+    if (err != 0) {
+        printf("Setting GPIO pin level failed: %d\n", err);
+        return -1;
+    }
+
+    printf("Toggling GPIO[%ld] to %s\n", idx, value ? "on" : "off");
+
+    return 0;
+}
+
+// コマンド登録
+SHELL_CMD_REGISTER(gpio, NULL, "Toggle GPIO", cmd_gpio_toggle);
+
+// メイン関数
 int main(void)
 {
-    printf("Hello Fuckin World!\n");
+    int err;
+
+    err = initialize_all_gpios();
+    if( err != 0 ) {
+        printf("GPIO initialization failed: %d\n", err);
+        return -1;
+    }
+
 	return 0;
 }
